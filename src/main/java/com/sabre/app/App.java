40f0fd54400bb05e69da210a0d5f3791635506b4
kornelM <makusz07@gmail.com>
2019-07-28 16:24:43 +0200

    Added main class

---
+++ src/main/java/com/sabre/app/App.java
@@ -0,0 +1,21 @@
+package com.sabre.app;
+
+import com.sabre.app.service.FileService;
+import com.sabre.app.service.WordService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App {
+    private static final Logger log = LogManager.getLogger(FileService.class);
+
+    public static void main(String[] args) {
+        log.info("App is starting...");
+        if (args.length != 0) {
+            WordService wordService = new WordService();
+            wordService.computeFile(args[0]);
+
+        } else {
+            log.error("No args given!");
+        }
+    }
+}
