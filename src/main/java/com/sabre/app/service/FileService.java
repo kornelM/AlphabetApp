e61e1b22d0aa6a66b34ed9d11972e3ad142d22cd
kornelM <makusz07@gmail.com>
2019-07-28 13:03:01 +0200

    Added FileService class implementation

--- src/main/java/com/sabre/app/service/FileService.java
+++ src/main/java/com/sabre/app/service/FileService.java
@@ -1,5 +1,30 @@
 package com.sabre.app.service;
 
-public class FileService {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileService {
+    private static final Logger log = LoggerFactory.getLogger(FileService.class);
+    private static final String EMPTY_STRING = "";
+
+    public String getStringFromFile(String pathToFile) {
+        String stringFromFile = EMPTY_STRING;
+        try (BufferedReader reader = new BufferedReader(new FileReader(pathToFile))) {
+            StringBuilder builder = new StringBuilder();
+            String currentLine = reader.readLine();
+
+            while (currentLine != null) {
+                builder.append(currentLine);
+                currentLine = reader.readLine();
+            }
+            stringFromFile = builder.toString();
+        } catch (IOException e) {
+            log.error("Could not read from file " + pathToFile, e);
+        }
+        return stringFromFile;
+    }
 }
