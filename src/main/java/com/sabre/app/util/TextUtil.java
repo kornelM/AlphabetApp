a29458ab6db3c58b6f6be0dfcc4c7593a11f9b93
kornelM <makusz07@gmail.com>
2019-07-28 16:23:24 +0200

    Extracted common variables to TextUtil class

---
+++ src/main/java/com/sabre/app/util/TextUtil.java
@@ -0,0 +1,10 @@
+package com.sabre.app.util;
+
+public class TextUtil {
+    public final static String PUNCTUATION_REGEX = "[^a-zA-Z0-9 ]";
+    public final static String NON_LETTERS_REGEX = "[^a-zA-Z ]";
+    public final static String EMPTY_STRING = "";
+    public final static char SPACE_CHAR = ' ';
+    public final static String SPACE_SEPARATOR = "\\s+";
+    public static final String COMMA = ",";
+}
